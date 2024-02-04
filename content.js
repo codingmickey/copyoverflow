fab5147be4a598075f6872247c11845883368a77
Kartik Jolapara <kjmickey003@gmail.com>
2024-02-05 01:57:03 +0530

    feat: Add copy and check button icons to code block
    Signed-off-by: Kartik Jolapara <kjmickey003@gmail.com>

--- content.js
+++ content.js
@@ -1,6 +1,13 @@
 window.addEventListener('load', main, false);
 
-document.querySelector('pre.s-code-block').addEventListener('');
+// TODO: add event listener to the code block when it laods onto the page
+// document.querySelector('pre.s-code-block').addEventListener('');
+
+const copyButtonIcon =
+  '<svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24" style="padding-top: 2px;"><path d="M0 0h24v24H0z" fill="none"></path><path d="M16 1H4c-1.1 0-2 .9-2 2v14h2V3h12V1zm3 4H8c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h11c1.1 0 2-.9 2-2V7c0-1.1-.9-2-2-2zm0 16H8V7h11v14z" fill="currentColor"></path></svg>';
+
+const checkButtonIcon =
+  '<svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24"><path d="M0 0h24v24H0z" fill="none" /> <path d="M9 16.2L4.8 12l-1.4 1.4L9 19 21 7l-1.4-1.4L9 16.2z" fill="currentColor" /></svg>';
 
 function main() {
   console.log('Script injected hehe :)');
@@ -13,10 +20,9 @@ function main() {
     const copyButton = document.createElement('button');
     // style="position: absolute;top: 10px;right: -191px;transition: right 0.1s ease-in-out 0s;background: transparent;border: 1px solid white;border: 0;height: 37px;width: 37px;box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1); /* Add box shadow */transition: background-color 0.3s, box-shadow 0.3s; /* Add transition for hover effect */color: white;border-radius: 10px;"
     copyButton.style =
-      'position: absolute;top: 10px;right: 10px;transition: right 0.1s ease-in-out 0s;background: transparent;border: 1px solid white;border: 0;height: 37px;width: 37px;box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1); /* Add box shadow */transition: background-color 0.3s, box-shadow 0.3s; /* Add transition for hover effect */color: white;border-radius: 10px;';
+      'position: absolute;top: 10px;right: 10px;transition: right 0.1s ease-in-out 0s;background: #1D1B1B;border: 1px solid rgb(73, 77, 80);height: 37px;width: 37px;box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1); /* Add box shadow */transition: background-color 0.3s, box-shadow 0.3s; /* Add transition for hover effect */color: white;border-radius: 10px;';
     copyButton.onclick = copyContent;
-    copyButton.innerHTML =
-      '<svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24" style="padding-top: 2px;"><path d="M0 0h24v24H0z" fill="none"></path><path d="M16 1H4c-1.1 0-2 .9-2 2v14h2V3h12V1zm3 4H8c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h11c1.1 0 2-.9 2-2V7c0-1.1-.9-2-2-2zm0 16H8V7h11v14z" fill="currentColor"></path></svg>';
+    copyButton.innerHTML = copyButtonIcon;
     codeBlock.appendChild(copyButton);
 
     codeBlock.style = 'position: relative;';
@@ -25,11 +31,15 @@ function main() {
 }
 
 function copyContent(event) {
+  event.stopPropagation();
+
   // Find the closest div to the button
   const btn = document.querySelector('button');
 
-  const button = event.target.parentNode;
-  const closestDiv = button.previousElementSibling; // Adjust if necessary
+  const button = event.target.closest('button');
+  const closestDiv = button.previousElementSibling;
+
+  console.log('closestDiv', closestDiv);
 
   if (closestDiv) {
     // Copy the content of the div
@@ -54,8 +64,17 @@ function copyContent(event) {
     // Remove the temporary textarea from the document
     document.body.removeChild(textarea);
 
+    // Change the button's icon
+    button.innerHTML = checkButtonIcon;
+    button.onclick = null;
+    // Remove the check mark after a delay (adjust duration accordingly)
+    setTimeout(() => {
+      button.innerHTML = copyButtonIcon;
+      button.onclick = copyContent;
+    }, 2000); // 2000 milliseconds (2 seconds) duration, adjust as needed
+
     // Optionally, provide feedback to the user
-    alert('Content has been copied to the clipboard!');
+    // alert('Content has been copied to the clipboard!');
   } else {
     alert('No adjacent div found!');
   }
